ea93c8e17f0dc563490ccdb3f0b202c7048d29fa
Foma <foma@shipilov.ru>
2024-06-04 10:05:50 +0300

    Recognize 1 digit codes

--- icdmappings/mappers/icd10_to_chapters.py
+++ icdmappings/mappers/icd10_to_chapters.py
@@ -9,7 +9,7 @@ from icdmappings.data_files import ICD10_CM_Chapters
 class ICD10toChapters(MapperInterface):
         """
         Maps ICD-10 CM diagnostic codes to ICD-10 chapters.
-        
+
         source of mapping: https://icd.who.int/browse10/2010/en
         """
         def __init__(self):
@@ -18,7 +18,7 @@ class ICD10toChapters(MapperInterface):
 
         def _setup(self):
             self.chapter_lookup = self._parse_file(self.filename)
-        
+
         def _map_single(self, icd10code : str) -> str:
             """
             Maps a single ICD-10 CM code in string format to its corresponding chapter.
@@ -36,24 +36,27 @@ class ICD10toChapters(MapperInterface):
 
             if not isinstance(icd10code, str): # has to be string
                 return None
-            
+
 
             letter = icd10code[0]
             if letter not in self.chapter_lookup:
                 return None
-            
+
             if len(icd10code) < 3:
                 return None
             try:
                 num = int(icd10code[1:3])
             except:
-                return None
-            
+                if icd10code[1].isdigit() and icd10code[2].isalpha():
+                    num = int(icd10code[1])
+                else:
+                    return None
+
             for candidate_chapter in self.chapter_lookup[letter]:
                 # use python's bisect to check if number is within bounds
                 if num >= candidate_chapter['range'][0] and num <= candidate_chapter['range'][1]:
                     return candidate_chapter['chapter']
-            
+
             return None
 
 
@@ -74,13 +77,13 @@ class ICD10toChapters(MapperInterface):
 
             if isinstance(icd10code, str):
                 return self._map_single(icd10code)
-            
+
             elif isinstance(icd10code, Iterable):
                 return[self._map_single(code) for code in icd10code]
-            
+
             return None
-          
-            
+
+
 
         def _parse_file(self, filename : str):
             """
